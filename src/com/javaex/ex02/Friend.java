2ea43accffd4cd59adab9f64683dac3d5f95e8de
m-ji97 <tv5035@naver.com>
2024-01-12 18:34:17 +0900

    24-01-12 숙제

---
+++ src/com/javaex/ex02/Friend.java
@@ -0,0 +1,22 @@
+package com.javaex.ex02;
+
+public class Friend<T> {
+
+    private String name;
+    private String hp;
+    private String school;
+	
+    public Friend() {
+    	
+	}
+
+	public Friend(String name, String hp, String school) {
+		this.name = name;
+		this.hp = hp;
+		this.school = school;
+	}
+    
+
+
+
+}
---
+++ src/com/javaex/ex02/FriendApp.java
@@ -0,0 +1,19 @@
+package com.javaex.ex02;
+
+import java.util.Scanner;
+
+public class FriendApp {
+
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);    	
+    	Friend<Friend> fList = new Friend<Friend>();
+    	
+    	
+    	Stirng name = sc.nextLine();
+
+
+    	
+    	
+    }
+
+}
